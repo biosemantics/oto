fe5caa3f9ba7e4d499c1010108bcdf646242305b
rodenhausen <thomas.rodenhausen@gmail.com>
2015-05-19 16:57:54 -0700

    update to support categorization per web service

---
+++ oto-common/src/main/java/edu/arizona/biosemantics/oto/common/model/CategorizeTerms.java
@@ -0,0 +1,38 @@
+package edu.arizona.biosemantics.oto.common.model;
+
+import java.io.Serializable;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class CategorizeTerms implements Serializable {
+
+	private DecisionHolder decisionHolder;
+	private String authenticationToken;
+	
+	public CategorizeTerms() { }
+	
+	public CategorizeTerms(DecisionHolder decisionHolder, String authenticationToken) {
+		super();
+		this.decisionHolder = decisionHolder;
+		this.authenticationToken = authenticationToken;
+	}
+
+	
+	public DecisionHolder getDecisionHolder() {
+		return decisionHolder;
+	}
+
+	public String getAuthenticationToken() {
+		return authenticationToken;
+	}
+
+	public void setDecisionHolder(DecisionHolder decisionHolder) {
+		this.decisionHolder = decisionHolder;
+	}
+
+	public void setAuthenticationToken(String authenticationToken) {
+		this.authenticationToken = authenticationToken;
+	}
+	
+}
---
+++ oto-common/src/main/java/edu/arizona/biosemantics/oto/common/model/CategoryBean.java
@@ -0,0 +1,62 @@
+package edu.arizona.biosemantics.oto.common.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * This class is to hold the data of a category
+ * 
+ * @author Fengqiong
+ * 
+ */
+@XmlRootElement
+public class CategoryBean implements Serializable {
+	private static final long serialVersionUID = -225751981230348851L;
+	private String name;
+	private String def;
+	private ArrayList<String> terms;
+	private ArrayList<Term> changed_terms;
+
+	public CategoryBean() { }
+	
+	public String getDef() {
+		return def;
+	}
+
+	public void setDef(String definition) {
+		this.def = definition;
+	}
+
+	public CategoryBean(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public ArrayList<String> getTerms() {
+		return terms;
+	}
+
+	public void setTerms(ArrayList<String> terms) {
+		this.terms = terms;
+	}
+
+	public void setChanged_terms(ArrayList<Term> changed_terms) {
+		this.changed_terms = changed_terms;
+	}
+
+	public ArrayList<Term> getChanged_terms() {
+		return changed_terms;
+	}
+	
+	
+}
+

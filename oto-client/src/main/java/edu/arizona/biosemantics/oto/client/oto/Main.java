8411540ff951aa62aa16c089f94fd0a088af51c5
rodenhausen <thomas.rodenhausen@gmail.com>
2015-05-28 13:28:31 -0700

    split model off of commons

--- oto-client/src/main/java/edu/arizona/biosemantics/oto/client/oto/Main.java
+++ oto-client/src/main/java/edu/arizona/biosemantics/oto/client/oto/Main.java
@@ -1,16 +1,22 @@
 package edu.arizona.biosemantics.oto.client.oto;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import edu.arizona.biosemantics.common.biology.TaxonGroup;
+import edu.arizona.biosemantics.oto.common.model.CategorizeTerms;
+import edu.arizona.biosemantics.oto.common.model.CategoryBean;
 import edu.arizona.biosemantics.oto.common.model.CreateDataset;
+import edu.arizona.biosemantics.oto.common.model.CreateUserResult;
+import edu.arizona.biosemantics.oto.common.model.DecisionHolder;
 import edu.arizona.biosemantics.oto.common.model.GlossaryDictionaryEntry;
 import edu.arizona.biosemantics.oto.common.model.GroupTerms;
 import edu.arizona.biosemantics.oto.common.model.Order;
 import edu.arizona.biosemantics.oto.common.model.StructureHierarchy;
+import edu.arizona.biosemantics.oto.common.model.Term;
 import edu.arizona.biosemantics.oto.common.model.TermContext;
 import edu.arizona.biosemantics.oto.common.model.TermOrder;
 import edu.arizona.biosemantics.oto.common.model.User;
@@ -24,15 +30,17 @@ public class Main {
 	 * @throws InterruptedException 
 	 */
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
-		OTOClient otoClient = new OTOClient("http://localhost:8080/otoOld2/");
+		OTOClient otoClient = new OTOClient("http://localhost:8080/oto/");
 
 		otoClient.open();
 		
 		String email = "thomas.rodenhausen@gmail.com";
+		/*String pass = "termspassword";
 		String firstName = "Thomas";
-		String lastName = "Rodenhausen";
-		String token = "9ImelotCiHj2FMQ8fK5B0w==";
+		String lastName = "Rodenhausen";*/
+		//String token = "9ImelotCiHj2FMQ8fK5B0w==";
 		//String token = "TA+QdXcpz+AUi1iOjZDvZw==";
+		String token = "4hbYwF7HCNljBGkHL38uRA==";
 		
 		/*User user = new User();
 		user.setUserEmail(email);
@@ -43,8 +51,13 @@ public class Main {
 		user.setBioportalUserId("asdf");
 		user.setBioportalApiKey("asdf2");
 		
-		Future<String> result = otoClient.postUser(user);
+		Future<CreateUserResult> result = otoClient.postUser(user);
+		System.out.println("token " + result.get());
+		
+		Future<String> result2 = otoClient.getUserAuthenticationToken(user);
+		System.out.println("token " + result2.get());
 		*/
+		
 		//List<TermContext> termContexts = new LinkedList<TermContext>();
 		//termContexts.add(new TermContext("a", "a sentence"));
 		
@@ -63,14 +76,121 @@ public class Main {
 		//String token = tokenResult.get();
 		//Future<String> result = otoClient.postDataset(new CreateDataset("acb", TaxonGroup.PLANT, token));
 		
+		String datasetName = otoClient.postDataset(new CreateDataset("th133", TaxonGroup.PLANT, token)).get();
+		System.out.println(datasetName);
+		
 		List<TermContext> termContexts = new LinkedList<TermContext>();
 		termContexts.add(new TermContext("d", "d sentence"));
 		termContexts.add(new TermContext("e", "e sentence"));
 		termContexts.add(new TermContext("c", "c sentence"));
 		
 		GroupTerms groupTerms = new GroupTerms(termContexts, token, true);
-		Future<GroupTerms.Result> result = otoClient.postGroupTerms("thomas_tester123", groupTerms);
+		Future<GroupTerms.Result> result = otoClient.postGroupTerms(datasetName, groupTerms);
 		System.out.println(result.get().getCount());
+		
+		CategorizeTerms categorizeTerms = new CategorizeTerms();
+		categorizeTerms.setAuthenticationToken(token);
+		DecisionHolder decisionHolder = new DecisionHolder();
+		ArrayList<CategoryBean> regularCategories = new ArrayList<CategoryBean>();
+		CategoryBean categoryBean = new CategoryBean();
+		categoryBean.setName("taste");
+		ArrayList<Term> changedTerms = new ArrayList<Term>();
+		Term term = new Term();
+		term.setAdditional(false);
+		term.setHasSyn(false);
+		term.setTerm("c");
+		
+		Term synTerm = new Term();
+		synTerm.setAdditional(false);
+		synTerm.setHasSyn(false);
+		synTerm.setTerm("d");
+		
+		changedTerms.add(term);
+		//term = new Term();
+		//term.setAdditional(true);
+		//term.set
+		changedTerms.add(synTerm);
+		categoryBean.setChanged_terms(changedTerms);		
+		
+		regularCategories.add(categoryBean);
+		decisionHolder.setRegular_categories(regularCategories);
+		categorizeTerms.setDecisionHolder(decisionHolder);
+		otoClient.postGroupTermsCategorization(datasetName, categorizeTerms);
+				
+		
+		CategorizeTerms categorizeTerms2 = new CategorizeTerms();
+		categorizeTerms2.setAuthenticationToken(token);
+		DecisionHolder decisionHolder2 = new DecisionHolder();
+		ArrayList<CategoryBean> regularCategories2 = new ArrayList<CategoryBean>();
+		CategoryBean categoryBean2 = new CategoryBean();
+		categoryBean2.setName("taste");
+		ArrayList<Term> changedTerms2 = new ArrayList<Term>();
+		Term term2 = new Term();
+		term2.setAdditional(false);
+		term2.setHasSyn(true);
+		term2.setTerm("c");
+		term2.setRelatedTerms("'d'");
+		ArrayList<Term> syns2 = new ArrayList<Term>();
+		Term synTerm2 = new Term();
+		synTerm2.setAdditional(false);
+		synTerm2.setHasSyn(false);
+		synTerm2.setTerm("d");
+		syns2.add(synTerm2);
+		term2.setSyns(syns2);
+		//term = new Term();
+		//term.setAdditional(true);
+		//term.set
+		Term oldTerm = new Term();
+		oldTerm.setAdditional(true);
+		oldTerm.setHasSyn(false);
+		oldTerm.setRelatedTerms("synonym of 'c'");
+		oldTerm.setTerm("d");
+		changedTerms2.add(oldTerm);
+		changedTerms2.add(term2);
+		categoryBean2.setChanged_terms(changedTerms2);
+				
+		regularCategories2.add(categoryBean2);
+		decisionHolder2.setRegular_categories(regularCategories2);
+		
+		categorizeTerms2.setDecisionHolder(decisionHolder2);
+		otoClient.postGroupTermsCategorization(datasetName, categorizeTerms2);
+		
+		
+		/*CategorizeTerms categorizeTerms = new CategorizeTerms();
+		categorizeTerms.setAuthenticationToken(token);
+		DecisionHolder decisionHolder = new DecisionHolder();
+		ArrayList<CategoryBean> regularCategories = new ArrayList<CategoryBean>();
+		CategoryBean categoryBean = new CategoryBean();
+		categoryBean.setName("taste");
+		ArrayList<Term> changedTerms = new ArrayList<Term>();
+		Term term = new Term();
+		term.setAdditional(false);
+		term.setHasSyn(true);
+		term.setTerm("c");
+		ArrayList<Term> syns = new ArrayList<Term>();
+		Term synTerm = new Term();
+		synTerm.setAdditional(true);
+		synTerm.setHasSyn(false);
+		synTerm.setTerm("d");
+		syns.add(synTerm);
+		term.setSyns(syns);
+		changedTerms.add(term);
+		//term = new Term();
+		//term.setAdditional(true);
+		//term.set
+		changedTerms.add(synTerm);
+		categoryBean.setChanged_terms(changedTerms);
+		ArrayList<String> terms = new ArrayList<String>();
+		terms.add("c");
+		terms.add("d");
+		categoryBean.setTerms(terms);
+		
+		
+		regularCategories.add(categoryBean);
+		decisionHolder.setRegular_categories(regularCategories);
+		
+		categorizeTerms.setDecisionHolder(decisionHolder);
+		otoClient.postGroupTermsCategorization("th123_rodenhausen_20150521094833", categorizeTerms);*/
 		
 		//OTOClient otoClient = new OTOClient("http://biosemantics.arizona.edu/OTO/");
 		//OTOClient otoClient = new OTOClient("http://localhost:9090/oto/");	
